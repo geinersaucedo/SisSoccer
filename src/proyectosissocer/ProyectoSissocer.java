b3b833dac7783c06c579df0b34c9d8135a641973
geinersaucedo <gsaucedo92@gmail.com>
2019-06-13 22:57:56 -0500

    Codigo Fuente Inicial

---
+++ src/proyectosissocer/ProyectoSissocer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectosissocer;
+
+/**
+ *
+ * @author Geiner Saucedo
+ */
+public class ProyectoSissocer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmPrincipal frm= new frmPrincipal();
+        
+        frm.setVisible(true);
+    }
+    
+}
